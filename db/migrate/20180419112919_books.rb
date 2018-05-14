aaada9a6fbb36932b19c9045f78d3b5ad7b8973e
rudrakshbhati <rudrakshbhati@gmail.com>
2018-05-14 19:20:08 +0530

    first commit

---
+++ db/migrate/20180419112919_books.rb
@@ -0,0 +1,11 @@
+class Books < ActiveRecord::Migration[5.2]
+  def change
+  	create_table :books do |t|
+  	   t.column :title, :string, :limit => 32, :null => false
+  	   t.column :price, :float
+  	   t.column :subject_id, :integer
+  	   t.column :description, :text
+  	   t.column :created_at, :timestamp
+  	end
+  end
+end
